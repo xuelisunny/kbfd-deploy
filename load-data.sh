b5ceaa1f3f5fa8c4321c921b2ced428e16c17fc8
snowysunny <xueli.hit@gmail.com>
2018-11-03 00:32:59 +0800

    a

--- load-data.sh
+++ load-data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-$dataset
 git add --all
 git commit -m "a"
 git push
@@ -8,7 +7,7 @@ for ((i=1;i<6;i++)); do
 		cd kbfd-deploy
 		git pull
 		cp data-expt.sh /home/ubuntu/run/data-expt.sh
-		nohup /home/ubuntu/run/data-expt.sh  $dataset > ~/load-data.txt 2>&1 &
+		nohup /home/ubuntu/run/data-expt.sh $dataset > ~/load-data.txt 2>&1 &
 		exit
 EOF
 done
