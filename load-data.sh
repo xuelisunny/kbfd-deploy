b63053b7d94f5661cf61c5b7a7ec9197d89507d7
snowysunny <xueli.hit@gmail.com>
2018-11-03 00:26:30 +0800

    a

--- load-data.sh
+++ load-data.sh
@@ -7,8 +7,8 @@ for ((i=1;i<6;i++)); do
 	ssh aws0$i <<EOF
 		cd kbfd-deploy
 		git pull
-		cp data-expt.sh /home/ubuntu/run/data-expt.sh $dataset
-		nohup /home/ubuntu/run/data-expt.sh > ~/load-data.txt 2>&1 &
+		cp data-expt.sh /home/ubuntu/run/data-expt.sh
+		nohup /home/ubuntu/run/data-expt.sh  $dataset > ~/load-data.txt 2>&1 &
 		exit
 EOF
 done
