82b15c123f9db4d4fd478e447fcffdbaad31f02f
snowysunny <xueli.hit@gmail.com>
2018-11-04 00:17:10 +0800

    a

--- execute.sh
+++ execute.sh
@@ -2,6 +2,9 @@
 command1='nohup pkill -9 java && cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./Mcoordinator.jar config-sample.properties 3 1 1 >/dev/null 2>&1 &  exit' 
 command2='pkill -9 java && cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./Mworker.jar aws01 config-sample.properties 3 1 1 >/dev/null 2>&1 &  exit' 
 command3='cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./client.jar aws01 >/dev/null 2>&1 &  exit'  
+git add --all
+git commit -m "a"
+git push
 ssh aws01 <<EOF
 	pkill -9 java
 	cd kbfd-deploy
