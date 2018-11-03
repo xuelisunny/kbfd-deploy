250be7384cfbb099d66264274267319672bb9b18
snowysunny <xueli.hit@gmail.com>
2018-11-03 12:32:43 +0800

    a

--- data-expt.sh
+++ data-expt.sh
@@ -2,7 +2,7 @@
 set timeout -1
 set dataset [lindex $argv 0]
 set password Lee692817
-spawn scp -r yli34@staff.ssh.inf.ed.ac.uk:$dataset /home/ubuntu/data/
+spawn rsync -r yli34@staff.ssh.inf.ed.ac.uk:$dataset /home/ubuntu/data/
 expect {
 	 	     "(yes/no)?"
 	         {
