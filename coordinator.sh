2b26925cd9899fb10ea2c21f079d3501975dc351
snowysunny <xueli.hit@gmail.com>
2018-11-04 10:49:22 +0800

    a

--- coordinator.sh
+++ coordinator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.security.policy=security.policy -Xms5G -Xmx20G  -Xmx10G -Xss10G -XX:PermSize=10G -XX:MaxPermSize=10G -jar ./Mcoordinator.jar config-sample.properties 4 1 1
+java -Djava.security.policy=security.policy -Xmx20G -jar ./Mcoordinator.jar config-sample.properties 4 1 1
