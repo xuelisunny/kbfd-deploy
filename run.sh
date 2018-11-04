70697529d7513f74d31b7c17236b693cffe7b8aa
snowysunny <xueli.hit@gmail.com>
2018-11-04 21:22:51 +0800

    a

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ chmod 400 aws_bj.pem aws_nx.pem
 
  ps -ef |grep yago4 | grep -v grep | awk  '{print $2}'|xargs kill -9 
 
-
+find . -name ".DS_Store" -type f -print -exec rm -rf {} \;
 
 pid=$(ssh root@$remote_host "ps -ef | grep data-expt | grep -v grep | awk  '{print $2}'"| awk '{print $2}')
 
