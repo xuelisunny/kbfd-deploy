af685208e3815b8fec51c74a63af318f39648a71
snowysunny <xueli.hit@gmail.com>
2018-11-05 09:02:26 +0800

    a

--- coordinator.sh
+++ coordinator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.security.policy=security.policy -Xmx15G -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump ./Mcoordinator.jar config-sample.properties 4 1 1
+java -Djava.security.policy=security.policy -Xmx15G -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/ubuntu/heapdump ./Mcoordinator.jar config-sample.properties 4 1 1
