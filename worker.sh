5b319163193f2bd26ad6d3e18d87245ff8e0eb37
snowysunny <xueli.hit@gmail.com>
2018-11-05 08:10:20 +0800

    a

--- worker.sh
+++ worker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.security.policy=security.policy -Xmx20G -jar ./Mworker.jar aws01 config-sample.properties 4 1 1 
+java -Djava.security.policy=security.policy -Xmx20G -jar -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump ./Mworker.jar aws01 config-sample.properties 4 1 1 
