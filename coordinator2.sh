1bbd19957bfec22b6fe43f91496dfdee6753b2f6
snowysunny <xueli.hit@gmail.com>
2018-11-06 14:50:04 +0800

    a

---
+++ coordinator2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Djava.security.policy=security.policy -Xmx25G -jar  ./Mcoordinator.jar config-sample2.properties 4 1 1
---
+++ worker2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Djava.security.policy=security.policy -Xmx15G -jar ./Mworker.jar aws10 config-sample2.properties 4 1 1 
