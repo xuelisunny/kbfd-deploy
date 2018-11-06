d895c07c1335bdbb09b11bb8a2c70caf2ebc7b46
snowysunny <xueli.hit@gmail.com>
2018-11-06 17:54:24 +0800

    a

--- execute.sh
+++ execute.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
-command1='nohup pkill -9 java && cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./Mcoordinator.jar config-sample.properties 3 1 1 >/dev/null 2>&1 &  exit' 
-command2='pkill -9 java && cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./Mworker.jar aws01 config-sample.properties 3 1 1 >/dev/null 2>&1 &  exit' 
-command3='cd kbfd-deploy && java -Djava.security.policy=security.policy -Xmx20G -jar ./client.jar aws01 >/dev/null 2>&1 &  exit'  
+command1='pkill -9 java && cd kbfd-deploy && git pull && nohup java -Djava.security.policy=security.policy -Xmx20G -jar ./Mcoordinator.jar config-sample.properties 3 1 1 > /home/ubuntu/coordinator.txt &  exit' 
+command2='pkill -9 java && cd kbfd-deploy && git pull && nohup java -Djava.security.policy=security.policy -Xmx15G -jar ./Mworker.jar aws01 config-sample.properties 3 1 1 > /home/ubuntu/worker.txt 2>&1 &  exit' 
+command3='cd kbfd-deploy && java -Djava.security.policy=security.policy -jar ./client.jar localhost > /home/ubuntu/client.txt 2>&1 &  exit'  
 git add --all
 git commit -m "a"
 git push
-ssh aws01 <<EOF
-	pkill -9 java
-	cd kbfd-deploy
-	git pull
-	cp coordinator.sh /home/ubuntu/run/coordinator.sh
-	chmod +x /home/ubuntu/run/coordinator.sh
-	nohup /home/ubuntu/run/coordinator.sh > /home/ubuntu/coordinator.txt 2>&1 &
-	exit
-EOF
+ssh aws01 command1
 for ((i=2;i<6;i++)); do
-	 ssh aws0$i <<EOF
-	 pkill -9 java
-	 cd kbfd-deploy
-	 git pull
-	 cp worker.sh /home/ubuntu/run/worker.sh
-	 chmod +x /home/ubuntu/run/worker.sh
-	 nohup /home/ubuntu/run/worker.sh > /home/ubuntu/worker.txt 2>&1 & 
-	 exit
-EOF
+	ssh aws0$i command2
 done
-ssh aws01 <<EOF
-	cd kbfd-deploy
-	git pull
-	cp client.sh /home/ubuntu/run/client.sh 
-	chmod +x /home/ubuntu/run/client.sh
-	nohup /home/ubuntu/run/client.sh > /home/ubuntu/client.txt 2>&1 & 
-	exit
-EOF
+ssh aws01 command3
