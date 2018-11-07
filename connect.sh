0a155589d82c51be2b84ac5770dbf30095a3ec5e
snowysunny <xueli.hit@gmail.com>
2018-11-07 20:40:36 +0800

    a

--- connect.sh
+++ connect.sh
@@ -1,5 +1,7 @@
 for ((i=1;i<=9;i++)); do
 	 ssh aws0$i <<EOF
+	 rm -rf ./data/dbpedia4
+	 rm -rf ./data/yago8
 	 pkill -9 java
 	 cd kbfd-deploy
 	 rm *.log
@@ -10,6 +12,8 @@ EOF
 done
 for ((i=10;i<=21;i++)); do
 	 ssh aws$i <<EOF
+	 rm -rf ./data/dbpedia4
+	 rm -rf ./data/yago8
 	 pkill -9 java
 	 cd kbfd-deploy
 	 rm *.log
