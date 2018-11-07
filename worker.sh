6c789f02ced28e56774aec46301922154f93bdf7
snowysunny <xueli.hit@gmail.com>
2018-11-07 17:44:37 +0800

    a

--- worker.sh
+++ worker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.security.policy=security.policy -Xmx15G -jar ./Mworker.jar aws01 config-sample.properties 4 1 1 
+java -Djava.security.policy=security.policy -Xmx15G -jar ./Mworker.jar aws21 config-sample.properties 4 1 1 
