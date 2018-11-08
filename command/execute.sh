4a1bf7f76aa5ece744405ab930908ea85c08e44f
snowysunny <xueli.hit@gmail.com>
2018-11-08 19:00:14 +0800

    a

--- command/execute.sh
+++ command/execute.sh
@@ -5,6 +5,7 @@ git push
 ssh aws05 <<EOF
 	pkill -9 java
 	cd kbfd-deploy
+	cp ./config/config /home/ubuntu/.ssh/
 	git pull
 	chmod +x ./jar/coordinator.sh 
 	nohup ./jar/coordinator.sh $1.properties $2 $3 > /home/ubuntu/coordinator.txt 2>&1 &
@@ -13,7 +14,8 @@ EOF
 for ((i=1;i<=4;i++)); do
 	 ssh aws0$i <<EOF
 	 pkill -9 java
-	 cd kbfd-deploy/jar
+	 cd kbfd-deploy
+     cp ./config/config /home/ubuntu/.ssh/
 	 git pull
 	 chmod +x /jar/worker.sh
 	 nohup ./jar/worker.sh $1.properties $2 $3 > /home/ubuntu/worker.txt 2>&1 & 
