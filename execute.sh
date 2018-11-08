321ae0ff5e7a87cc0e5aff61954b4b64837c9167
snowysunny <xueli.hit@gmail.com>
2018-11-08 22:05:32 +0800

    a

--- execute.sh
+++ execute.sh
@@ -2,14 +2,14 @@
 git add --all
 git commit -m "a"
 git push
-ssh aws05 <<EOF
+ssh aws01 <<EOF
 	pkill -9 java
 	cd kbfd-deploy
 	git pull
 	nohup ./coordinator.sh $1.properties $2 $3 > /home/ubuntu/coordinator.txt 2>&1 &
 	exits
 EOF
-for ((i=1;i<=4;i++)); do
+for ((i=2;i<=5;i++)); do
 	 ssh aws0$i <<EOF
 	 pkill -9 java
 	 cd kbfd-deploy
