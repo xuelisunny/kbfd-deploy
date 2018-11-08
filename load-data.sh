3279a1b0bc9614ea212732dd81ed781402a15713
snowysunny <xueli.hit@gmail.com>
2018-11-08 17:12:49 +0800

    a

--- load-data.sh
+++ load-data.sh
@@ -6,15 +6,10 @@ echo $path
 git add --all
 git commit -m "a"
 git push
-for ((i=1;i<=9;i++)); do
+for ((i=1;i<=5;i++)); do
 	ssh aws0$i <<EOF
-		nohup /home/ubuntu/run/data-expt.sh $path > ~/load-data.txt 2>&1 &
-		exit
-EOF
-done
-for ((i=10;i<=21;i++)); do
-	ssh aws$i <<EOF
-		nohup /home/ubuntu/run/data-expt.sh $path > ~/load-data.txt 2>&1 &
+	    chmod +x /home/ubuntu/kbfd-deploy/data-expt.sh
+		nohup /home/ubuntu/kbfd-deploy/data-expt.sh $path > ~/load-data.txt 2>&1 &
 		exit
 EOF
 done
