c26974af4c679a2bd837cd696b58277b6aa634f8
snowysunny <xueli.hit@gmail.com>
2018-11-08 23:44:42 +0800

    a

--- coordinator.sh
+++ coordinator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -Djava.security.policy=security.policy -Xmx100G -jar  ./Mcoordinator.jar $1 $2 $3 >> /home/ubuntu/coordinator.txt &
+nohup java -Djava.security.policy=security.policy -Xmx100G -jar  ./Mcoordinator.jar $1 $2 $3 > /home/ubuntu/coordinator.txt 2>&1 &
