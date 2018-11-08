93d09708ab7134fa831205b0b6640cfc50edac39
snowysunny <xueli.hit@gmail.com>
2018-11-08 18:58:07 +0800

    a

--- command/execute.sh
+++ command/execute.sh
@@ -2,26 +2,26 @@
 git add --all
 git commit -m "a"
 git push
-ssh aws01 <<EOF
+ssh aws05 <<EOF
 	pkill -9 java
 	cd kbfd-deploy
 	git pull
-	chmod +x ./coordinator.sh 
-	nohup ./coordinator.sh $1.properties $2 $3 > /home/ubuntu/coordinator.txt 2>&1 &
+	chmod +x ./jar/coordinator.sh 
+	nohup ./jar/coordinator.sh $1.properties $2 $3 > /home/ubuntu/coordinator.txt 2>&1 &
 	exits
 EOF
-for ((i=2;i<=5;i++)); do
+for ((i=1;i<=4;i++)); do
 	 ssh aws0$i <<EOF
 	 pkill -9 java
-	 cd kbfd-deploy
+	 cd kbfd-deploy/jar
 	 git pull
-	 chmod +x worker.sh
-	 nohup ./worker.sh $1.properties $2 $3 > /home/ubuntu/worker.txt 2>&1 & 
+	 chmod +x /jar/worker.sh
+	 nohup ./jar/worker.sh $1.properties $2 $3 > /home/ubuntu/worker.txt 2>&1 & 
 	 exit
 EOF
 done
 ssh aws01 <<EOF
-	cd kbfd-deploy
-	nohup ./client.sh > /home/ubuntu/client.txt 2>&1 & 
+	cd kbfd-deploy/jar
+	nohup ./jar/client.sh > /home/ubuntu/client.txt 2>&1 & 
 	exit
 EOF
