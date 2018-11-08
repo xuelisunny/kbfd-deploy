31cef6f13197a5dae0d07e4a4aa675295d558609
snowysunny <xueli.hit@gmail.com>
2018-11-08 22:58:43 +0800

    a

--- execute.sh
+++ execute.sh
@@ -4,24 +4,29 @@ git commit -m "a"
 git push
 ssh aws01 <<EOF
 	pkill -9 java
-	cd kbfd-deploy
-	git pull
-	chmod +x ./coordinator.sh
+	mkdir run
+	cp kbfd-deploy/coordinator.sh run/
+	cp kbfd-deploy/Mcoordinator.jar run/
+	cd run
+	chmod +x coordinator.sh
 	sh ./coordinator.sh ${1}.properties $2 $3
 	exit
 EOF
 for ((i=2;i<=5;i++)); do
 	 ssh aws0$i <<EOF
 	 pkill -9 java
-	 cd kbfd-deploy
-	 git pull
-	 chmod +x ./worker.sh
+	 cp kbfd-deploy/worker.sh run/
+	 cp kbfd-deploy/Mworker.jar run/
+	 cd run
+	 chmod +x worker.sh
 	 sh ./worker.sh ${1}.properties $2 $3
 	 exit
 EOF
 done
 ssh aws01 <<EOF
-	cd kbfd-deploy
+    cp kbfd-deploy/client.* run/
+	cd run
+	chmod +x client.sh
 	sh ./client.sh
 	exit
 EOF
