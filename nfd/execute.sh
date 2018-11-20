c6b25cf7d09b9b6dfa81272f56f624f4f6f5d69b
xuelisunny <xueli.hit@gmail.com>
2018-11-20 12:14:00 +0800

    a

--- nfd/execute.sh
+++ nfd/execute.sh
@@ -4,9 +4,12 @@ git commit -m "a"
 git push
 ssh aws01 <<EOF
 	pkill -9 java
+	rm ~/run/*
 	cd kbfd-deploy
 	cp security.policy ~/run/
 	git pull
+	cd nfdrm ~/rurm ~/run/*rm ~/run/*rm ~/run/*n/*
+	cp *.jar *.sh *.properties ~/run/
 	cd ~/run/
 	sudo chmod +x *.sh
 	sh ./coordinator.sh ${1}.properties $2 $3 $4 $5 $6
@@ -15,8 +18,11 @@ EOF
 for i in {2,3,4,5,6,7,8,9}; do
 	 ssh aws0$i <<EOF
 		 pkill -9 java
-		 cd kbfd-deploy
+		 cd kbfd-rm ~/run/*rm ~/run/*deploy
+		 rm ~/run/*
 		 git pull
+		 cd nfd
+	     cp *.jar *.sh *.properties ~/run/
 		 cp security.policy ~/run/
 		 cd ~/run/
 		 sh ./worker.sh ${1}.properties $2 $3 $4 $5 $6 $7
--- nfd/worker.sh
+++ nfd/worker.sh
@@ -1,1 +1,1 @@
-nohup java -Djava.security.policy=security.policy -jar ./frag-worker.jar aws01 $1 $2 $3 $4 $5 $6 $7 >> /home/ubuntu/worker.txt 2>&1 &
+nohup java -Djava.security.policy=security.policy -jar ./frag-worker.jar aws01 $1 $2 $3 $4 $5 $6 $7 > /home/ubuntu/worker.txt 2>&1 &
