8903b1fac6b4dfa6c47ce5e14b85e909ba690839
xuelisunny <xueli.hit@gmail.com>
2018-11-20 11:18:03 +0800

    a

--- nfd/execute.sh
+++ nfd/execute.sh
@@ -17,17 +17,17 @@ ssh aws01 <<EOF
 EOF
 for i in {2,3,4,5,6,7,8,9}; do
 	 ssh aws0$i <<EOF
-	 pkill -9 java
-	 cd kbfd-deploy
-	 git pull
-	 cp security.policy ~/run/
-	 cd nfd
-	 rm ~/run/*
-	 sudo cp *.sh *.properties *.jar ~/run/
-	 sudo chmod +x *.sh
-	 cd ~/run/
-	 sh ./worker.sh ${1}.properties $2 $3 $4 $5 $6 $7
-	 exit
+		 pkill -9 java
+		 cd kbfd-deploy
+		 git pull
+		 cp security.policy ~/run/
+		 cd nfd
+		 rm ~/run/*
+		 sudo cp *.sh *.properties *.jar ~/run/
+		 sudo chmod +x *.sh
+		 cd ~/run/
+		 sh ./worker.sh ${1}.properties $2 $3 $4 $5 $6 $7
+		 exit
 EOF
 done
 ssh aws01 <<EOF
