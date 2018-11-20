e3fc13c95e3a56e547182e702ae5c4440bbf68cb
xuelisunny <xueli.hit@gmail.com>
2018-11-20 22:35:56 +0800

    a

--- batchrun.sh
+++ batchrun.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 sh ./execute.sh dbpedia 8 2 15 1 400 4 
 echo "begin to sleep to wait"
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 5
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 6
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 7
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 8
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 9
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
 sh ./execute.sh dbpedia 8 2 15 1 400 10
-sleep 500s
+sleep 50s
 sh ./test.sh
 sleep 2s
