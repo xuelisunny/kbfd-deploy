4ba71c469dd8d168d27a874f05f74b1719febb2f
xuelisunny <xueli.hit@gmail.com>
2018-11-20 16:58:37 +0800

    a

---
+++ batchrun.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sh ./execute.sh dbpedia 8 2 15 1 400 4 
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 5
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 6
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 7
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 8
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 9
+sleep 500s
+sh ./test.sh
+sleep 2s
+sh ./execute.sh dbpedia 8 2 15 1 400 10
+sleep 500s
+sh ./test.sh
+sleep 2s
